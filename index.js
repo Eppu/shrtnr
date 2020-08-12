51971bcb870a83ad819152d4f32b7f14dafa7c34
Eetu Eskelinen <edwardtehgreat@gmail.com>
2020-08-12 22:10:46 +0300

    add first version of api and broken frontend

--- index.js
+++ index.js
@@ -1,30 +1,91 @@
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+
+require('dotenv').config();
+
+
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get('urls');
+urls.createIndex({ slug: 1 }, { unique: true });
+
 
 const app = express();
-
-app.use(helmet());
+app.enable('trust proxy');
 app.use(morgan('tiny'));
-app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
+app.use(helmet());
+
 
 app.get('/url/:id', (req, res) => {
     // get short url by id 
     
 });
 
-app.get('/:id', (req, res) => {
+app.get('/:id', async (req, res) => {
     // Redirect to url
-
+    const { id: slug } = req.params;
+    try {
+        const url = await urls.findOne({ slug });
+        if (url) {
+            res.redirect(url.url);
+        }
+        res.redirect(`/?error=${slug} not found`);
+    } catch (error) {      
+        res.redirect(`/?error=Link not found`);
+    }
 });
 
-app.post('/url', (req, res) => {
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
+});
+
+app.post('/url', async (req, res, next) => {
     // create a short url
-    
+    let { slug, url } = req.body;
+    try {
+        // Validate input with schema
+        await schema.validate({
+            slug,
+            url
+        });
+        // if slug doesn't exist, create one
+        if(!slug) {
+            slug = nanoid(5);
+        } else {
+            const existing = await urls.findOne({ slug });
+            if(existing) {
+                throw new Error('Slug already in use. 🐌');
+            }
+        }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        };
+        const created = await urls.insert(newUrl);
+        res.json(created);
+    } catch (error) {
+        next(error);
+    }  
 });
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack
+    })
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
